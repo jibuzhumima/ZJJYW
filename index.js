5d9b1993e7bac561a35930e11e6d9d5878a746c5
jibuzhumima <105899013+jibuzhumima@users.noreply.github.com>
2022-05-20 10:51:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function change(myid,mode){
+
+    document.getElementById(myid).style.display=mode;
+
+    // if (mode != 'block'){
+    //     document.getElementById(myid).style.display="";
+    // }
+}

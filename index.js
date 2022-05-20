b7f8c94b72627ebcc3b5a9f1bbec60bd4b6fee0a
jibuzhumima <105899013+jibuzhumima@users.noreply.github.com>
2022-05-20 21:32:18 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function change(myid,mode){
-
-    document.getElementById(myid).style.display=mode;
-
-    // if (mode != 'block'){
-    //     document.getElementById(myid).style.display="";
-    // }
-}
